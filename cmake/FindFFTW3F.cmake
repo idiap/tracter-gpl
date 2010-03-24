b2ebbbf7dcb98ed2cf0e608384d5f3306a2be6ff
Phil Garner <Phil.Garner@idiap.ch>
2010-03-24 18:38:15 +0100

    Move to CMake

---
+++ cmake/FindFFTW3F.cmake
@@ -0,0 +1,9 @@
+#
+# Find FFTW3F
+#
+# Phil Garner
+# March 2010
+#
+include(FindPkgConfig)
+
+pkg_check_modules(FFTW3F fftw3f)
---
+++ cmake/FindSamplerate.cmake
@@ -0,0 +1,9 @@
+#
+# Find Samplerate
+#
+# Phil Garner
+# March 2010
+#
+include(FindPkgConfig)
+
+pkg_check_modules(SAMPLERATE samplerate)
